8af3f3751a31706a0b5c959a068e958613ca95c6
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-04-03 16:25:39 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,4 +4,12 @@ const refs = {
   stopBtn: document.querySelector('button[data-stop]'),
 };
 
-console.log(refs)
+// console.log(refs)
+
+function getStart() {
+  refs.startBtn.setAttribute("disabled", "true");
+  refs.stopBtn.removeAttribute("disabled");
+  timerId = setInterval(() => {document.body.style.background = getRandomHexColor();
+  }, 1000);
+  console.log('change color');
+}
