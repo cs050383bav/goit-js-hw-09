00c44f0ed158ad236478a82d14e6683055352e14
cs050383bav <87935875+cs050383bav@users.noreply.github.com>
2022-04-03 16:01:03 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+import '../css/common.css';
 
+/*
+ - Создание
+ - Unix-время
+ - Методы
+ - Разница времени
+ - Date.now()
+*/
+
+const date = new Date();
+console.log(date)
+
+const date1 = Date.now();
+console.log('date1', date1);
+
+setTimeout(() => {
+  const date2 = Date.now();
+
+  console.log('date1', date1);
+  console.log('date2', date2);
+
+  console.log(date2 - date1);
+}, 3000);
